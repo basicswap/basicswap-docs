2c084fb6b400367048deb03e7882ddc8758e0087
Gerlof van Ek <gerlof@particl.io>
2025-04-07 23:46:08 +0200

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            Get Started 📚️
+            Get Started
           </Link>
         </div>
       </div>
